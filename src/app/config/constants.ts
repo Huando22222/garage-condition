30868994757ec6d7c1fe22893c19155acf6538bc
HuanDo22222 <huando22222@gmail.com>
2024-04-10 02:52:22 +0700

    update deploy

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -1,7 +1,7 @@
 
 export enum Constants {
-  URL = 'http://localhost:3000/',
+  URL = 'https://garage-condition-be-deploy.onrender.com/',
   //   URL = 'https://garage-condition-be.onrender.com/',
-  URL_IMG = 'http://localhost:3000/images/',
+  URL_IMG = 'https://garage-condition-be-deploy.onrender.com/images/',
   //   URL_IMG = 'https://garage-condition-be.onrender.com/images/',
 }
